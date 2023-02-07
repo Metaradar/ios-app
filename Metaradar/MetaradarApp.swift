6b78a155bd100924a147554268a25d7841d9e59e
Ralph Küpper <ralph.kuepper@skelpo.com>
2023-02-07 16:53:54 +0100

    Initial Commit

---
+++ Metaradar/MetaradarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaradarApp.swift
+//  Metaradar
+//
+//  Created by Ralph Küpper on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaradarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
